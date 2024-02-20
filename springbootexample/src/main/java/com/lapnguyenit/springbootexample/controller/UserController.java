ca24105b2917a15aa56b8f4a3c797d839dba3dfc
lapnguyen <21130419@st.hcmuaf.edu.vn>
2024-02-20 10:02:34 +0700

    bài 44 :truyền tham số @RequestParam @PathVarible @RequestBody

--- springbootexample/src/main/java/com/lapnguyenit/springbootexample/controller/UserController.java
+++ springbootexample/src/main/java/com/lapnguyenit/springbootexample/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.lapnguyenit.springbootexample.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.lapnguyenit.springbootexample.model.User;
+import netscape.javascript.JSObject;
+import org.springframework.web.bind.annotation.*;
 
 /*
 * định nghĩa url /user*/
@@ -13,12 +13,25 @@ public class UserController {
     public String getUser(){
         return "lapnguyen";
     }
-    @GetMapping("/lapnguyen")
+    @RequestMapping("/lapnguyen")
     public String getInfor(){
         return "lap 24 tuoi";
     }
-    @GetMapping("/getUser")
+    @RequestMapping("/getUser")
     public String addUser(){
         return "da them";
+    }
+    @GetMapping("getInfor/{id}") //chỉ có trong spring boot nhận tham số trên url khác với @RequestParam, dựa vào template trong url
+    public String getUserInfor(@PathVariable("id") int id){
+        return "mã:" +id;
+    }
+    @GetMapping("getName")
+    public String getName (@RequestParam("name") String name){ //nhận tham số trên url giống với jsp/servlet ?name=
+        return "tên:" + name;
+    }
+//    tham số truyền dưới dạng 1 đối tượng json
+    @PostMapping("update")
+    public String update(@RequestBody User user){
+        return "update" + user.getId() + user.getUsername() + user.getPassword();
     }
 }
